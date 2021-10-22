013e101852ec00b09516c50821ccef796b9aabf4
armando <jorgearmandou@gmail.com>
2021-10-21 21:51:30 -0500

    se modifica generacion de pdf usando cell y truncadno la descripcion a solo 25 caracteres

--- src/pdf/generar.php
+++ src/pdf/generar.php
@@ -2,9 +2,10 @@
 require_once '../../conexion.php';
 require_once 'fpdf/fpdf.php';
 //$pdf = new FPDF('P', 'mm', 'letter');
-$pdf = new FPDF('P', 'mm', array(80, 150));
+$pdf = new FPDF('P', 'mm', array(80, 130));
 $pdf->AddPage();
-$pdf->SetMargins(3, 5, 3);
+$pdf->SetMargins(3, 0, 3);
+$pdf->SetAutoPageBreak(true,0);
 $pdf->SetTitle("Ventas");
 //$pdf->SetFont('Arial', 'B', 12);
 $pdf->SetFont('Arial', 'B', 10);
@@ -53,29 +54,30 @@ $pdf->SetFont('Arial', '', 8);
 $pdf->Cell(60, 5, utf8_decode($datosC['direccion']), 0, 1, 'L');
 
 $pdf->Ln(3);
-$pdf->SetFont('Arial', 'B', 7);
+$pdf->SetFont('Arial', 'B', 8);
 $pdf->SetTextColor(255, 255, 255);
 $pdf->Cell(75, 5, "Detalle de Producto", 1, 1, 'C', 1);
 $pdf->SetTextColor(0, 0, 0);
 $pdf->Cell(5, 5, utf8_decode('N°'), 0, 0, 'C');
-$pdf->Cell(27, 5, utf8_decode('Descripción'), 0, 0, 'L');
-$pdf->Cell(8, 5, 'Cant', 0, 0, 'C');
+$pdf->Cell(33, 5, utf8_decode('Descripción'), 0, 0, 'L');
+$pdf->Cell(7, 5, 'Cant', 0, 0, 'C');
 $pdf->Cell(15, 5, 'Vl.Unit', 0, 0, 'C');
-$pdf->Cell(20, 5, 'Sub Total.', 0, 1, 'C');
+$pdf->Cell(15, 5, 'Sub Total.', 0, 1, 'C');
 $pdf->SetFont('Arial', '', 7);
 $contador = 1;
 $x = $pdf->GetX();
 $y = $pdf->GetY();
 $total = 0;
 while ($row = mysqli_fetch_assoc($ventas)) {
-    //$pdf->Cell(5, 5, $contador, 0, 0, 'R');
+    $pdf->Cell(5, 5, $contador, 0, 0, 'R');
     //$pdf->MultiCell(27, 5, $row['descripcion'], 1, 'L', false);
-    /*$pdf->Cell(27, 5, 'este es un produto  de nombre',0, 0, 'L');
-    $pdf->Cell(8, 5, $row['cantidad'], 0, 0, 'R');
+    $pdf->Cell(33, 5, substr($row['descripcion'],0 , 25),0, 0, 'L');
+    $pdf->Cell(7, 5, $row['cantidad'], 0, 0, 'R');
     $pdf->Cell(15, 5, number_format($row['precio'], 0, '.', ','), 0, 0, 'R');
-    $pdf->Cell(20, 5, number_format($row['cantidad'] * $row['precio'], 0, '.', ','), 0, 1, 'R');*/
+    $pdf->Cell(15, 5, number_format($row['cantidad'] * $row['precio'], 0, '.', ','), 0, 1, 'R');
 
-    $pdf->MultiCell(5, 5, $contador, 1, 'R', false);
+
+    /*$pdf->MultiCell(5, 5, $contador, 1, 'R', false);
     $x+=5;
     $pdf->SetXY($x,$y);
     $pdf->MultiCell(27, 5, $row['descripcion'], 1, 'L', false);
@@ -87,15 +89,17 @@ while ($row = mysqli_fetch_assoc($ventas)) {
     $pdf->MultiCell(15, 5, number_format($row['precio'], 0, ',', '.'),1 , 'R', false);
     $x+=15;
     $pdf->SetXY($x,$y);
-    $pdf->MultiCell(20, 5, number_format($row['cantidad'] * $row['precio'], 0, ',', '.'),1 , 'R', false);
+    $pdf->MultiCell(20, 5, number_format($row['cantidad'] * $row['precio'], 0, ',', '.'),1 , 'R', false);*/
     $contador++;
     //$pdf->Ln();
     $x=$pdf->GetX();
     $y+=5;
     $total+=$row['cantidad']*$row['precio'];
 }
-$pdf->Cell(55, 5, 'TOTAL:', 1, 0, 'R');
-$pdf->Cell(20, 5, '$ '.number_format($total, 0, ',', '.'), 1, 0, 'R');
+$pdf->SetFont('Arial', 'B', 7);
+$pdf->Cell(53, 5, 'TOTAL:', 0, 0, 'R');
+$pdf->SetFont('Arial', 'B', 7);
+$pdf->Cell(22, 5, '$ '.number_format($total, 0, ',', '.'), 0, 0, 'R');
 $pdf->Output("ventas.pdf", "I");
 
 ?>
